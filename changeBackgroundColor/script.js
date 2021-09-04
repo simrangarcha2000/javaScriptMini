15c6a8530742614b55eead3353f1447b00cf665c
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-09-04 12:49:11 -0400

    Background color project done

--- changeBackgroundColor/script.js
+++ changeBackgroundColor/script.js
@@ -1,3 +1,8 @@
+const color = ['red', 'blue', 'green', 'white', 'pink', 'yellow', 'purple']
+
+
 changeBackground = ()=>{
     console.log('change Background')
+    const randomColor = parseInt(Math.random() * color.length)
+    document.body.style.backgroundColor = color[randomColor]
 }
